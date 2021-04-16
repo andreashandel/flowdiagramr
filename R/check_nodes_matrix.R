ba865a6f51ae1c5fb760723300da5e4b7bd20981
Andrew Tredennick <atredennick@west-inc.com>
2021-04-16 13:59:48 -0600

    Updates and renames nodes placement checker to work with new implementation as a matrix rather than a data frame.

---
+++ R/check_nodes_matrix.R
@@ -0,0 +1,17 @@
+#' Checks for coherence between the node matrix and the input list.
+#'
+#' @param input_list A list.
+#' @param nodes_matrix A data frame.
+#' @noRd
+
+check_nodes_matrix <- function(input_list, nodes_matrix) {
+  ret_message <- paste0("The inputs list contains variable (nodes) not ",
+                        "contained in the nodes matrix, or vice versa.")
+
+  in_vars <- sort(input_list$varlabels)
+  mat_vars <- sort(nodes_matrix[nodes_matrix != ""])
+  flag <- isTRUE(all.equal.character(in_vars, df_vars))
+  if(flag == FALSE) {
+    stop(ret_message)
+  }
+}
